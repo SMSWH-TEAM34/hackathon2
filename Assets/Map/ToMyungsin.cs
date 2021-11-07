1def12b06c09217e70edde78e42d5e68c3bb7267
Sorrel98 <wp05110@naver.com>
2021-11-07 16:26:04 +0900

    worldmap2_fin

--- Assets/Map/ToMyungsin.cs
+++ Assets/Map/ToMyungsin.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ToMyungsin : MonoBehaviour
 {
+    public GameObject ui;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,19 @@ public class ToMyungsin : MonoBehaviour
     void Update()
     {
         
+    }
+    public void myungsinClikced()
+    {
+        SceneManager.LoadScene("junghyun");
+    }
+
+    public void baekClicked()
+    {
+        SceneManager.LoadScene("Mission3");
+    }
+
+    public void noClicked()
+    {
+        ui.SetActive(false);
     }
 }
